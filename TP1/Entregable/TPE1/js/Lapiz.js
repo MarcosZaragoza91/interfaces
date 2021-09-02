9ab041ad497316e850f18bd5c55a72a152738ed1
ptome@summasolutions.net <ptome@summasolutions.net>
2021-09-02 10:56:49 -0300

    Se crearon funciones basicas para dibujar, se crea el objeto Lapiz

---
+++ TP1/Entregable/TPE1/js/Lapiz.js
@@ -0,0 +1,41 @@
+class Lapiz {
+    constructor() {
+        this.coordenadas = {x:0,y:0};
+        this._color = '#000000';
+        this._forma = 'circle';
+        this._grosor = 5;
+    }
+
+    getColor() {
+        return this._color;
+    }
+
+    setColor(value) {
+        this._color = value;
+    }
+
+    getForma() {
+        return this._forma;
+    }
+
+    setForma(value) {
+        this._forma = value;
+    }
+
+    getGrosor() {
+        return this._grosor;
+    }
+
+    setGrosor(value) {
+        this._grosor = value;
+    }
+
+    getCoordenada(){
+        return this.coordenadas;
+    }
+
+    setCoordenada(x,y){
+        this.coordenadas.x = x;
+        this.coordenadas.y = y;
+    }
+}
