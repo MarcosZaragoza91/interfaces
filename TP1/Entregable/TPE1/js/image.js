79f211ae27ffe1e0d8be492a3982e2f5f88929f0
ptome@summasolutions.net <ptome@summasolutions.net>
2021-09-03 20:22:51 -0300

    Funcion para cargar imagenes al canvas

--- TP1/Entregable/TPE1/js/image.js
+++ TP1/Entregable/TPE1/js/image.js
@@ -1,20 +1,21 @@
-let img = new Image();
+let input = document.getElementById("imagen");
+let canva = document.getElementById('myCanvas');
+let contex = canva.getContext("2d");
 
-// document.querySelector('#imagen').addEventListener('click',()=>{
-//     img.src =
-// })
-document.getElementById("cargar").addEventListener('click',mostrar);
-
-function mostrar(){
-    var archivo = document.getElementById("imagen").value;
-//    var img = new Image();
-    if (archivo) {
-      img.src = archivo;
-      ctx.drawImage(img,10,10);
-      // img.onload = function () {
-      // }
+input.onchange = e => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = readerEvent => {
+        if (file.type.match('image.*')) {
+            let content = readerEvent.target.result;
+            let image = new Image();
+            image.src = content;
+            image.onload = function () {
+                canva.width = image.width;
+                canva.heigth = image.heigth;
+                contex.drawImage(image,0,0);
+            }
+        }
     }
-  }
-
-
-  
+}
