717bc6269568fd2361d6257d0ea59afb66a19fe0
Marcos Zaragoza <marcos.zaragoza73@hotmail.com>
2021-09-07 18:18:22 -0300

    se agrego background-image hmtl

---
+++ TP1/Entregable/TPE1/js/Imagen.js
@@ -0,0 +1,41 @@
+class Imagen{
+
+    constructor() {
+        this.original = new Image();
+    }
+
+    cargarImagen(e){
+
+        let input = document.getElementById("imagen");
+        let canva = document.getElementById('myCanvas');
+        let contex = canva.getContext("2d");
+        let original = new Image();
+        input.onchange = e => {
+            let file = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = readerEvent => {
+                if (file.type.match('image.*')) {
+                    let content = readerEvent.target.result;
+                    original.src = content;
+                    original.onload = function () {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        canva.width = original.width;
+                        canva.heigth = original.heigth;
+                        contex.drawImage(original,0,0);
+                        this.setOriginal(original);
+                    }
+                }
+            }
+        }
+    }
+
+    getOriginal() {
+        return this.original;
+    }
+
+    setOriginal(value) {
+        this.original = value;
+    }
+
+}
