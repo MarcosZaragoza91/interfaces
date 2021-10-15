3f789b22764b57b473aaa7493343f6fd89642499
ptome@summasolutions.net <ptome@summasolutions.net>
2021-10-15 20:02:55 -0300

    TPE3- Definimos las clases Juego.js, Personaje.js y Obstaculo.js

---
+++ TP3/Entregable/TPE3/js/Obstaculo.js
@@ -0,0 +1,14 @@
+class Obstaculo{
+
+    constructor() {
+        this._esPremio = false;
+    }
+
+    getEsPremio() {
+        return this._esPremio;
+    }
+
+    setEsPremio(value) {
+        this._esPremio = value;
+    }
+}
