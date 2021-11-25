76da80f5dcdd729042466d31e6bbd8af02ff2d4a
marcosZaragoza <marcos.zaragoza7302@gmail.com>
2021-11-25 19:20:53 -0300

    se agrega videollamada

--- TP5/Entregable/js/js.js
+++ TP5/Entregable/js/js.js
@@ -1,27 +1,19 @@
 document.addEventListener("DOMContentLoaded",function(){
-    //Tomo los elementos
-   /*
-    let modal = document.querySelector(".modal");  
-   
-    let btn = document.querySelector(".btn-submit-2");
-        btn.addEventListener('click', function (){
-        modal.classList.remove("modal-oculto");
-          modal.classList.add("modal-registrarse");
-    });
-    
-    let btnCerrarForm = document.querySelector(".icon-cerrar-registrarse");
-    btnCerrarForm.addEventListener('click', function (){ 
-        modal.classList.remove("modal-registrarse");
-          modal.classList.add("modal-oculto");
-    });
-    */
+/*
     let mensajes = document.querySelector(".mensajes");
+    
     let btnMjsRecibido = document.querySelector(".btn-msj-recibidos");
     btnMjsRecibido.addEventListener('click', function (){ 
         mensajes.classList.remove("container-msj-oculto");
-          mensajes.classList.add("container-msj");
+        mensajes.classList.add("container-msj");
     });
-
-
+*/
+let mensajes = document.querySelector(".mensajes");
+    let btnCerrarMsj = document.querySelector(".icon-cerrar-msj");
+    
+    btnCerrarMsj.addEventListener('click', function (){ 
+        mensajes.classList.remove("container-msj");
+        mensajes.classList.add("container-msj-oculto");
+    });
 
 });
---
+++ TP5/Entregable/js/registrarse.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    let modal = document.querySelector(".modal");  
+       
+    let btn = document.querySelector(".btn-submit-2");
+    
+    btn.addEventListener('click', function (){
+        modal.classList.remove("modal-oculto");
+        modal.classList.add("modal-registrarse");
+    });
+    
+    let btnCerrarForm = document.querySelector(".icon-cerrar-registrarse");
+    
+    btnCerrarForm.addEventListener('click', function (){ 
+        modal.classList.remove("modal-registrarse");
+        modal.classList.add("modal-oculto");
+    });
+
+});
