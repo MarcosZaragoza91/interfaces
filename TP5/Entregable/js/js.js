ccdc3336ed446097d3c502e66928daf884cb94ad
marcosZaragoza <marcos.zaragoza7302@gmail.com>
2021-11-25 16:47:04 -0300

    se agrega funcionalidad al icono mensaje

--- TP5/Entregable/js/js.js
+++ TP5/Entregable/js/js.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded",function(){
     //Tomo los elementos
+   /*
     let modal = document.querySelector(".modal");  
    
     let btn = document.querySelector(".btn-submit-2");
-    btn.addEventListener('click', function (){
+        btn.addEventListener('click', function (){
         modal.classList.remove("modal-oculto");
           modal.classList.add("modal-registrarse");
     });
@@ -13,8 +14,14 @@ document.addEventListener("DOMContentLoaded",function(){
         modal.classList.remove("modal-registrarse");
           modal.classList.add("modal-oculto");
     });
+    */
+    let mensajes = document.querySelector(".mensajes");
+    let btnMjsRecibido = document.querySelector(".btn-msj-recibidos");
+    btnMjsRecibido.addEventListener('click', function (){ 
+        mensajes.classList.remove("container-msj-oculto");
+          mensajes.classList.add("container-msj");
+    });
 
-  
 
 
 });
